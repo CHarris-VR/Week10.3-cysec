adc6cd19f00d3e1a7af6f83695a8fd2b98486cd5
CHarris-VR <charrisagent@gmail.com>
2026-01-22 19:04:06 -0500

    finished making changes and got a status code 401

--- cmdb_api_audit.py
+++ cmdb_api_audit.py
@@ -1,6 +1,8 @@
 import requests
 
+# had to check for correct import settings
 API_URL = 'https://my.api.mockaroo.com/ironclad/cmdb.json'
 
 response = requests.get(API_URL, timeout=10)
-print("Status code:", response.status_code)
+print("Status code:", response.status_code) # Success 1
+
